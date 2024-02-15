10175023a78103557eb2f7deb7a2ee238bb1fe08
Kirill Zagurny <perfecto.grosso@gmail.com>
2024-02-15 18:09:38 +0300

    configure spring

---
+++ src/test/java/org/example/tregulovspring/TregulovSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tregulovspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TregulovSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
