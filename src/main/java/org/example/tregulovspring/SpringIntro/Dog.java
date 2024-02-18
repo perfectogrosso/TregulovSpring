cabc06d232c8df1ec7ef9b0f5ec6437206a79793
Kirill Zagurny <perfecto.grosso@gmail.com>
2024-02-18 15:49:32 +0300

    pass lesson 12

--- src/main/java/org/example/tregulovspring/SpringIntro/Dog.java
+++ src/main/java/org/example/tregulovspring/SpringIntro/Dog.java
@@ -2,11 +2,21 @@ package org.example.tregulovspring.SpringIntro;
 
 public class Dog implements Pet{
 
+    private String name;
+
     public Dog(){
         System.out.println("Dog Bean is created");
     }
     @Override
     public void say(){
         System.out.println("Bow-wow!");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
