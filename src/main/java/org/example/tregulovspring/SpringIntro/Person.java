d5bb8a217468f14787a7a89ff4ff6fc796261acf
Kirill Zagurny <perfecto.grosso@gmail.com>
2024-02-18 14:07:44 +0300

    pass lesson 8

--- src/main/java/org/example/tregulovspring/SpringIntro/Person.java
+++ src/main/java/org/example/tregulovspring/SpringIntro/Person.java
@@ -3,13 +3,21 @@ package org.example.tregulovspring.SpringIntro;
 public class Person {
     private Pet pet;
 
-    public Person(Pet pet){
+//    public Person(Pet pet){
+//        System.out.println("Person Bean is created");
+//        this.pet = pet;
+//    }
+
+    public Person(){
         System.out.println("Person Bean is created");
-        this.pet = pet;
     }
 
     public void callYoutPet(){
         System.out.println("Hello, my lovely pet!");
         pet.say();
+    }
+    public void setPet(Pet pet) {
+        this.pet = pet;
+        System.out.println("Class Person: set Pet");
     }
 }
